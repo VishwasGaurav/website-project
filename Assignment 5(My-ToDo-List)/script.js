735b424d27fc1f782343be2315110d5cdf279e53
Vishwas Gaurav <vishwashsinghf100@gmail.com>
2026-07-07 02:46:28 +0530

    Update script.js

--- Assignment 5(My-ToDo-List)/script.js
+++ Assignment 5(My-ToDo-List)/script.js
@@ -1,1 +1,20 @@
+function addTask() {
+    let input = document.getElementById("taskInput");
+    let task = input.value.trim();
 
+    if (task === "") {
+        alert("Please enter a task");
+        return;
+    }
+
+    let li = document.createElement("li");
+    li.innerHTML = `${task} <span class="delete" onclick="removeTask(this)">❌</span>`;
+
+    document.getElementById("taskList").appendChild(li);
+
+    input.value = "";
+}
+
+function removeTask(element) {
+    element.parentElement.remove();
+}
